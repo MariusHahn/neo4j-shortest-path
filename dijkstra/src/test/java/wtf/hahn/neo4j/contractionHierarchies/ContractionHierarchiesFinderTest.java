a15f8c82bc8117071f9d32807b1cdeda3c567125
Marius Hahn <marius.hahn@live.com>
2022-12-29 21:28:10 +0100

    Test Ch finder

--- dijkstra/src/test/java/wtf/hahn/neo4j/contractionHierarchies/ContractionHierarchiesFinderTest.java
+++ dijkstra/src/test/java/wtf/hahn/neo4j/contractionHierarchies/ContractionHierarchiesFinderTest.java
@@ -1,17 +1,18 @@
 package wtf.hahn.neo4j.contractionHierarchies;
 
 import static java.util.List.of;
-import static wtf.hahn.neo4j.contractionHierarchies.ProcedureTest.sourceTargetAtoFQuery;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BasicEvaluationContext;
 import org.neo4j.graphalgo.WeightedPath;
+import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.internal.helpers.collection.Iterables;
 import wtf.hahn.neo4j.util.IntegrationTest;
-
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ContractionHierarchiesFinderTest extends IntegrationTest {
 
@@ -22,17 +23,25 @@ public class ContractionHierarchiesFinderTest extends IntegrationTest {
     @Test
     void sourceTargetCypher() {
         try (Transaction transaction = database().beginTx()) {
-            String cypher = "CALL wtf.hahn.neo4j.contractionHierarchies.createContractionHierarchiesIndex('ROAD', 'cost')";
-            transaction.execute(cypher);
-            Map<String, Object> result = transaction.execute(sourceTargetAtoFQuery()).next();
-            Double pathCost = ((Double) result.get("pathCost"));
-            WeightedPath path = ((WeightedPath) result.get("path"));
-            Assertions.assertEquals(160.0, pathCost);
+            new ContractionHierarchiesIndexer(
+                    relationshipType().name()
+                    , costProperty()
+                    , transaction
+                    , Comparator.<Node>comparingInt(Node::getDegree).reversed()
+            ).insertShortcuts();
+            WeightedPath path = new ContractionHierarchiesFinder(new BasicEvaluationContext(transaction, database())
+            ).find(
+                    transaction.findNode(() -> "Location", "name", "A")
+                    , transaction.findNode(() -> "Location", "name", "F")
+                    , relationshipType()
+                    , costProperty()
+            );
+            Assertions.assertEquals(160.0, path.weight());
             String names = Iterables.stream(path.nodes())
                     .map(n -> n.getProperty("name").toString())
                     .collect(Collectors.joining(","));
             System.out.println(names);
-            System.out.printf("Result: %s%n", pathCost);
+            System.out.printf("Result: %s%n", path.weight());
             transaction.rollback();
         }
     }
