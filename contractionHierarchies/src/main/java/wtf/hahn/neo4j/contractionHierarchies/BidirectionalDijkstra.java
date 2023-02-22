cde851e2a8f1346994a08a24c4be0784627c65ba
Marius Hahn <marius.hahn@live.com>
2023-02-22 07:47:43 +0100

    save point BidirectionalDijkstra

--- contractionHierarchies/src/main/java/wtf/hahn/neo4j/contractionHierarchies/BidirectionalDijkstra.java
+++ contractionHierarchies/src/main/java/wtf/hahn/neo4j/contractionHierarchies/BidirectionalDijkstra.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
+import org.eclipse.collections.impl.factory.Sets;
 import org.neo4j.graphalgo.EvaluationContext;
 import org.neo4j.graphalgo.impl.util.PathImpl;
 import org.neo4j.graphalgo.impl.util.WeightedPathImpl;
@@ -23,27 +24,63 @@ public record BidirectionalDijkstra(EvaluationContext evaluationContext, PathExp
 
     public Path findShortestPath(final Node startNode, final Node endNode, final String costProperty) {
         final Transaction transaction = evaluationContext.transaction();
-        final TraversalDescription frowardDescription = transaction.traversalDescription()
-                .depthFirst()
-                .evaluator(Evaluators.toDepth(1))
-                .expand(upwardsExpander);
+        final TraversalDescription frowardDescription = getBaseDescription(transaction).expand(upwardsExpander);
+        final TraversalDescription backwardDescription = getBaseDescription(transaction).expand(upwardsExpander.reverse());
         final Map<Node, ExpandNode> forwardHeap = new HashMap<>();
+        final Map<Node, ExpandNode> backwardHeap = new HashMap<>();
         final PriorityQueue<ExpandNode> forwardQueue = new PriorityQueue<>();
+        final PriorityQueue<ExpandNode> backwardQueue = new PriorityQueue<>();
         forwardQueue.offer(new ExpandNode(null, startNode, 0.0));
-        while (!forwardQueue.isEmpty() && !forwardHeap.containsKey(endNode)) {
-            final ExpandNode expandNode = forwardQueue.poll();
-            final Traverser traverse = frowardDescription.traverse(expandNode.node);
-            for (Path path : traverse) {
-                if (path.length() == 0) continue;
-                final Number cost = getProperty(path.lastRelationship(), costProperty);
-                final Double distance = expandNode.distance + cost.doubleValue();
-                final ExpandNode e = new ExpandNode(path.lastRelationship(), path.endNode(), distance);
-                forwardQueue.offer(e);
-                System.out.println(e);
-                updateHeap(forwardHeap, distance, e);
+        backwardQueue.offer(new ExpandNode(null, endNode, 0.0));
+        while (
+                (!forwardQueue.isEmpty() || !backwardQueue.isEmpty())
+                && (!forwardHeap.containsKey(endNode) || !backwardHeap.containsKey(startNode))
+                && !commonDistanceIsShortest(forwardQueue, backwardQueue, forwardHeap, backwardHeap)
+        ) {
+            if ((!forwardQueue.isEmpty() && !backwardQueue.isEmpty()) && forwardQueue.peek().distance() < backwardQueue.peek().distance()
+                    || !forwardQueue.isEmpty()) {
+                expand(costProperty, frowardDescription, forwardHeap, forwardQueue);
+            } else {
+                expand(costProperty, backwardDescription, backwardHeap, backwardQueue);
             }
         }
+        if (forwardHeap.containsKey(endNode)) {
+            return resolvePath(startNode, endNode, forwardHeap);
+        } else if (backwardHeap.containsKey(startNode)) {
+            return resolvePath(endNode, startNode, backwardHeap);
+        }
         return resolvePath(startNode, endNode, forwardHeap);
+    }
+
+    private static boolean commonDistanceIsShortest(PriorityQueue<ExpandNode> forQ, PriorityQueue<ExpandNode> backQ, final Map<Node, ExpandNode> h1, final Map<Node, ExpandNode> h2) {
+        return Sets.intersect(h1.keySet(), h2.keySet()).stream().findFirst().map(commonNode -> {
+            double minDistForward = forQ.stream().mapToDouble(ExpandNode::distance).min().orElse(Double.MAX_VALUE);
+            double minDistBackward = backQ.stream().mapToDouble(ExpandNode::distance).min().orElse(Double.MAX_VALUE);
+            double commonDistance = h1.get(commonNode).distance() + h2.get(commonNode).distance();
+            return minDistForward >= commonDistance && minDistBackward >= commonDistance;
+        }).orElse(false);
+
+    }
+
+    private static void expand(String costProperty, TraversalDescription description,
+                               Map<Node, ExpandNode> heap, PriorityQueue<ExpandNode> queue) {
+        final ExpandNode expandNode = queue.poll();
+        final Traverser traverse = description.traverse(expandNode.node);
+        for (Path path : traverse) {
+            if (path.length() == 0) continue;
+            final Number cost = getProperty(path.lastRelationship(), costProperty);
+            final Double distance = expandNode.distance + cost.doubleValue();
+            final ExpandNode e = new ExpandNode(path.lastRelationship(), path.endNode(), distance);
+            queue.offer(e);
+            System.out.println(e);
+            updateHeap(heap, distance, e);
+        }
+    }
+
+    private static TraversalDescription getBaseDescription(Transaction transaction) {
+        return transaction.traversalDescription()
+                .depthFirst()
+                .evaluator(Evaluators.toDepth(1));
     }
 
     private static void updateHeap(final Map<Node, ExpandNode> heap, final Double distance, final ExpandNode expandNode) {
@@ -57,7 +94,7 @@ public record BidirectionalDijkstra(EvaluationContext evaluationContext, PathExp
         }
     }
 
-    private Path resolvePath(final Node startNode, final Node endNode, final Map<Node, ExpandNode> heap) {
+    private static Path resolvePath(final Node startNode, final Node endNode, final Map<Node, ExpandNode> heap) {
         final Stack<ExpandNode> relationships = new Stack<>();
         ExpandNode expandNode = heap.get(endNode);
         while (expandNode != null && expandNode.in != null) {
