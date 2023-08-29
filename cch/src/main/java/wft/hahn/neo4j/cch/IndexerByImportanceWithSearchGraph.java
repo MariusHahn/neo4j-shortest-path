d5b2dfdff5c4b39f6a20fe5cb48b6c34d6888dad
Marius Hahn <marius.hahn@live.com>
2023-08-29 13:36:22 +0200

    small refactoring

--- cch/src/main/java/wft/hahn/neo4j/cch/IndexerByImportanceWithSearchGraph.java
+++ cch/src/main/java/wft/hahn/neo4j/cch/IndexerByImportanceWithSearchGraph.java
@@ -20,6 +20,7 @@ public final class IndexerByImportanceWithSearchGraph {
     private final RelationshipType type;
     private final String costProperty;
     private final VertexLoader vertexLoader;
+    private int insertionCounter = 0;
 
     public IndexerByImportanceWithSearchGraph(String type, String costProperty, Transaction transaction) {
         this.type = RelationshipType.withName(type);
@@ -40,19 +41,16 @@ public final class IndexerByImportanceWithSearchGraph {
         final Vertex[] inNodes = nodeToContract.inNeighbors().filter(n -> n.rank == Vertex.UNSET).toArray(Vertex[]::new);
         final Vertex[] outNodes = nodeToContract.outNeighbors().filter(n -> n.rank == Vertex.UNSET).toArray(Vertex[]::new);
         final Collection<Shortcut> shortcuts = new ConcurrentLinkedDeque<>();
-        for (final Vertex inNode : inNodes) {
-            for (final Vertex outNode : outNodes) if (inNode != outNode) {
+        for (final Vertex inNode : inNodes) for (final Vertex outNode : outNodes) if (inNode != outNode) {
                 final Arc from = inNode.outArcs().stream().filter(arc -> nodeToContract.equals(arc.end)).findFirst().orElseThrow();
                 final Arc to = outNode.inArcs().stream().filter(arc -> nodeToContract.equals(arc.start)).findFirst().orElseThrow();
                 shortcuts.add(new Shortcut(from, to, from.weight + to.weight,from.hopLength + to.hopLength));
             }
-        }
         final int edgeDifference = shortcuts.size() - inNodes.length - outNodes.length;
         return new EdgeDifferenceAndShortcuts(nodeToContract, edgeDifference, shortcuts);
     }
 
     public Vertex insertShortcuts() {
-        int insertionCounter = 0;
         Set<Vertex> vertices = vertexLoader.loadAllVertices(costProperty, type);
         LastInsertWinsPriorityQueue<EdgeDifferenceAndShortcuts> queue =
                 new LastInsertWinsPriorityQueue<>(vertices.stream().map(this::shortcutsToInsert));
@@ -64,29 +62,31 @@ public final class IndexerByImportanceWithSearchGraph {
             vertexToContract.rank = rank;
             vertexLoader.setRankProperty(vertexToContract, rank++, type.name()+"_rank");
             for (Shortcut shortcut : poll.shortcuts) {
-                Vertex from = shortcut.in.start;
-                Vertex to = shortcut.out.end;
-                Arc existing = from.outArcs().stream().filter(arc -> arc.end.equals(to)).findFirst().orElse(null);
-                if (existing != null) {
-                    if (shortcut.weight < existing.weight) {
-                        existing.weight = shortcut.weight;
-                        existing.hopLength = shortcut.hopLength;
-                        existing.middle = vertexToContract;
-                        System.out.println("Update: "+ existing);
-                    }
-                } else {
-                    Arc arc = new Arc(from, to, shortcut.weight, vertexToContract, shortcut.hopLength);
-                    shortcut.in.start.addArc(arc);
-                    shortcut.out.end.addArc(arc);
-                    insertionCounter++;
-                    System.out.println(arc);
-                }
+                createOrUpdateEdge(vertexToContract, shortcut);
             }
             updateNeighborsInQueue(queue, vertexToContract);
         }
         vertexLoader.commit();
         System.out.println(insertionCounter + " shortcuts inserted!");
         return vertexToContract;
+    }
+
+    private void createOrUpdateEdge(Vertex vertexToContract, Shortcut shortcut) {
+        final Vertex from = shortcut.in.start;
+        final Vertex to = shortcut.out.end;
+        final Arc existing = from.outArcs().stream().filter(arc -> arc.end.equals(to)).findFirst().orElse(null);
+        if (existing != null) {
+            if (shortcut.weight < existing.weight) {
+                existing.weight = shortcut.weight;
+                existing.hopLength = shortcut.hopLength;
+                existing.middle = vertexToContract;
+            }
+        } else {
+            Arc arc = new Arc(from, to, shortcut.weight, vertexToContract, shortcut.hopLength);
+            shortcut.in.start.addArc(arc);
+            shortcut.out.end.addArc(arc);
+            insertionCounter++;
+        }
     }
 
     @RequiredArgsConstructor
@@ -109,6 +109,5 @@ public final class IndexerByImportanceWithSearchGraph {
                        vertexToContract.arcs().mapToInt(arc -> arc.hopLength).sum())
                     ;
         }
-
     }
 }
