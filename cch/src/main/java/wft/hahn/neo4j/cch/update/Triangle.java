31c0ca8714c45b2ad0af0ab8cee0ad88d3da3c36
Marius Hahn <marius.hahn@live.com>
2023-10-28 09:27:53 +0200

    add middle node to triangle

--- cch/src/main/java/wft/hahn/neo4j/cch/update/Triangle.java
+++ cch/src/main/java/wft/hahn/neo4j/cch/update/Triangle.java
@@ -1,8 +1,9 @@
 package wft.hahn.neo4j.cch.update;
 
 import wft.hahn.neo4j.cch.model.Arc;
+import wft.hahn.neo4j.cch.model.Vertex;
 
-public record Triangle(Arc a, Arc b, Arc c) {
+public record Triangle(Arc a, Arc b, Arc c, Vertex middle) {
     public double weight() {
         return b.weight + c.weight;
     }
