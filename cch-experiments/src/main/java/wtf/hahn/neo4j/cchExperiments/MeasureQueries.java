de1d16ab14ee225a7bbe5045576f6f72c7f5647d
Marius Hahn <marius.hahn@live.com>
2023-11-08 16:16:33 +0100

    assert fail on wrong path length

--- cch-experiments/src/main/java/wtf/hahn/neo4j/cchExperiments/MeasureQueries.java
+++ cch-experiments/src/main/java/wtf/hahn/neo4j/cchExperiments/MeasureQueries.java
@@ -65,7 +65,7 @@ public class MeasureQueries {
                     val dijkstraPath = dijkstraPaths.get(to);
                     val chPath = new StoppedResult<>(() -> diskChDijkstra.find(x, y));
                     if (dijkstraPath != null) {
-                        if (chPath.getResult().weight() != dijkstraPath.weight()) System.out.println(chPath.getResult().weight() + "vs: " + dijkstraPath.weight());
+                        if (chPath.getResult().weight() != dijkstraPath.weight()) throw new AssertionError(chPath.getResult().weight() + "vs: " + dijkstraPath.weight());
                         String measure = String.format("%9d,%9d,%9d,%9d,%4d,%4d%n",
                                 getId(from), getId(to), chPath.getMicros(), chPath.getResult().weight()
                                 , chPath.getResult().length()
